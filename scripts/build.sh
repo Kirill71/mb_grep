6ff02f9af9e19737cfce6ed8e1c7feba7424d774
Kyrylo Zharenkov <kyrylo.zharenkov@gmail.com>
2025-06-09 14:53:01 +0200

    Fix build system on MacOS

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,9 @@
 
 . scripts/env.sh
 
+JOBS=$(sysctl -n hw.ncpu 2>/dev/null || nproc)
+
 cmake -DCMAKE_BUILD_TYPE=Debug  -G "Unix Makefiles" -S "$SOURCE_DIR" -B "$DEBUG_BUILD_DIR"
 cmake -DCMAKE_BUILD_TYPE=Release  -G "Unix Makefiles" -S "$SOURCE_DIR" -B "$RELEASE_BUILD_DIR"
-cmake --build cmake-build-debug --target all -j"$(nproc)"
-cmake --build cmake-build-release --target all -j"$(nproc)"
+cmake --build cmake-build-debug --target all -j"$JOBS"
+cmake --build cmake-build-release --target all -j"$JOBS"
